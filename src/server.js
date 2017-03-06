0451e05564ba9887b7e1366d03ce7de1a39caac1
Madison Smith <ms8565@rit.edu>
2017-03-06 16:29:04 -0500

    Beginnings of color swatches

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const http = require('http');
 const fs = require('fs');
@@ -21,7 +21,7 @@ console.log(`Listening on 127.0.0.1: ${port}`);
 const io = socketio(app);
 
 let currentTriangles = [];
-let currentColors = ['black'];
+const currentColors = ['black'];
 
 let firstUser = true;
 
@@ -31,14 +31,13 @@ const onJoined = (sock) => {
   socket.on('join', () => {
     socket.join('room1');
   });
-  
-    if(firstUser){
-        socket.emit('setupFirstCanvas', null);
-        firstUser = false;
-    }
-    else{
-        socket.emit('updateCanvas', {triangles: currentTriangles});
-    }
+
+  if (firstUser) {
+    socket.emit('setupFirstCanvas', null);
+    firstUser = false;
+  } else {
+    socket.emit('updateCanvas', { triangles: currentTriangles });
+  }
 };
 
 const onDisconnect = (sock) => {
@@ -53,23 +52,23 @@ const onDraw = (sock) => {
   const socket = sock;
 
   socket.on('draw', (data) => {
-    for(var i = 0; i < data.triangles.length; i++){
-        currentTriangles[data.triangles[i].id] = data.triangles[i];
+    for (let i = 0; i < data.triangles.length; i += 1) {
+      currentTriangles[data.triangles[i].id] = data.triangles[i];
     }
     io.sockets.in('room1').emit('updateCanvas', data);
   });
 };
 
 const onUpdate = (sock) => {
-    const socket = sock;
-    
-    socket.on('updateTriangles', (data) => {
-        currentTriangles = data.triangles;
-    });
-    socket.on('updateColors', (data) => {
-        currentColors.push(data.newColor)
-        io.sockets.in('room1').emit('updateColors', currentColors);
-    });
+  const socket = sock;
+
+  socket.on('updateTriangles', (data) => {
+    currentTriangles = data.triangles;
+  });
+  socket.on('addSwatch', (data) => {
+    currentColors.push(data.newColor);
+    io.sockets.in('room1').emit('updateSwatches', { swatch: data.newColor });
+  });
 };
 
 io.sockets.on('connection', (socket) => {
